e6121e187d2079355ec6b0c2fe2e83325de792b4
ivm <stan777.durand777@gmail.com>
2025-01-08 18:11:08 +0100

    update final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: './public',  // Le dossier source pour les fichiers Vite (src)
   build: {
-    outDir: 'dist',  // Le dossier où les fichiers compilés seront placés après la construction
+    outDir: '../dist',  // Le dossier où les fichiers compilés seront placés après la construction
   },
   base: '/exp_threejs/',
 });
